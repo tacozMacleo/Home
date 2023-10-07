9bf13ad7a95d76bf95ff88269c9327812620c73e
Michael Bach Kristensen <michael@seluxit.com>
2023-10-07 16:51:18 +0200

    TMux - Added config file & link.

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ln -s ${BASEDIR}/src/.bashrc.d ~/.
+ln -s ${BASEDIR}/src/.tmux.conf ~/.
 ln -s ${BASEDIR}/src/.vimrc ~/.
 ln -s ${BASEDIR}/src/.vim ~/.
 ln -s ${BASEDIR}/src/.scripts ~/.
