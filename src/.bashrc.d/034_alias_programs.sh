64f5ad07dd638ca9ccac220e77936cf171cfdb30
Michael Bach Kristensen <tacoz666@gmail.com>
2025-09-27 13:18:33 +0200

    Gemini - Added as a alias.

--- src/.bashrc.d/034_alias_programs.sh
+++ src/.bashrc.d/034_alias_programs.sh
@@ -1,3 +1,6 @@
 alias lazydocker='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v /yourpath:/.config/jesseduffield/lazydocker lazyteam/lazydocker'
 
 alias decolor='sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"'
+
+alias gemini='npx https://github.com/google-gemini/gemini-cli'
+
