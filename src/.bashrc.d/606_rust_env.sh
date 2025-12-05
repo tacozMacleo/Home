95d773ae48a20ecacf251e46c5442b3a9e5e43df
Michael Bach Kristensen <michael.back@wirtek.com>
2025-12-05 11:50:19 +0100

    Bashrc - Updated the Rust env enabling script.

--- src/.bashrc.d/606_rust_env.sh
+++ src/.bashrc.d/606_rust_env.sh
@@ -1,1 +1,11 @@
-. $HOME/.cargo/env
+add_path(){
+    NEW_PATH=$1
+    if ! [[ "$PATH" =~ "$NEW_PATH" ]]
+    then
+        export PATH="$NEW_PATH:$PATH"
+    fi
+}
+# . ~/.cargo/env
+
+add_path "~/.cargo/bin"
+
